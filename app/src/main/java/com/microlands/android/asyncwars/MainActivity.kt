cce0122e829c94317dcda705479cab9afac80ae2
LuiX <916632+luix@users.noreply.github.com>
2020-02-01 23:54:50 -0800

    Download image using an Executor

--- app/src/main/java/com/microlands/android/asyncwars/MainActivity.kt
+++ app/src/main/java/com/microlands/android/asyncwars/MainActivity.kt
@@ -84,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
     //region --------- Modify below to setup app to use a specific type of async method --------- //
     val doProcessingOnUiThread = false // true
-    val methodToUse = MethodToDownloadImage.Handler    //Thread
+    val methodToUse = MethodToDownloadImage.Executor    //Thread
     //endregion
 
     // Setup the UI text
